ff393c31400e38e964deaf40fe029aa07268ca53
jwozniak <john.wozniak@banyanhills.com>
2019-09-27 07:07:40 -0400

    started adding to the main file for the service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -14,7 +14,21 @@ type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 }
 
+// Repository fake database for now
 type Repository struct {
-	myMutex      sync.RWMutex
+	myMutex      sync.RWMutextex
 	consignments []*pb.Consignment
+}
+
+//Create - creates a new Consignment
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.myMutex.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.myMutex.Unlock()
+	return consignment, nil
+}
+
+type service struct {
+	repo repository
 }
---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/johnwoz123/shippy-shippy-consignment
